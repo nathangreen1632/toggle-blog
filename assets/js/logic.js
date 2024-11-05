0e2e256c392c2f4689056aabeb308451c55a16e3
ngreen <faux_lucks0v@icloud.com>
2024-11-05 07:17:25 -0600

    Update core JS files

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,28 +1,33 @@
 function toggleLightDarkMode() {
+
   const currentMode = localStorage.getItem('mode') || 'light';
 
-  const newMode = currentMode === 'light' ? 'dark' : 'light';
+const isLightMode = currentMode === 'light'
+  const newMode = isLightMode ? 'dark' : 'light';
   const emoji = newMode === 'light' ? '🌙' : '🌞';
 
   document.body.className = newMode;
+  document.documentElement.setAttribute("style", "--circle-color: #93301b");
+
+  if (newMode === 'dark') {
+    document.documentElement.setAttribute("style", "--circle-color: #93301b");
+  } else {
+    document.documentElement.setAttribute("style", "--circle-color: #ffb100");
+  }
 
   localStorage.setItem('mode', newMode);
 }
 
-(function setInitialMode() {
-  const savedMode = localStorage.getItem('mode') || 'light';
-  document.body.className = savedMode;
-})();
-
 document.getElementById('toggle').addEventListener('click', toggleLightDarkMode);
 
 function readLocalStorage() {
+
   const blogData = JSON.parse(localStorage.getItem('blogPosts')) || [];
   return blogData;
 }
 
 function storeLocalStorage(newPost) {
-  const existingPosts = readLocalStorage();
+  const existingPosts = readLocalStorage('blogPosts');
 
   existingPosts.push(newPost);
 
@@ -34,3 +39,8 @@ const redirectPage = function (url) {
   redirectURL = url;
   location.assign(url);
 };
+
+(function setInitialMode() {
+  const savedMode = localStorage.getItem('mode') || 'light';
+  document.body.className = savedMode;
+})();
