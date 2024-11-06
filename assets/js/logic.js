c59d824f3bc5543f250acc199b3b4aba387a8317
ngreen <faux_lucks0v@icloud.com>
2024-11-06 11:30:53 -0600

    Update logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,23 +1,42 @@
 function toggleLightDarkMode() {
+
   const currentMode = localStorage.getItem('mode') || 'light';
-  const newMode = currentMode === 'light' ? 'dark' : 'light';
+
+const isLightMode = currentMode === 'light'
+  const newMode = isLightMode ? 'dark' : 'light';
+  const emoji = newMode === 'light' ? '🌙' : '🌞';
+
   document.body.className = newMode;
+  document.documentElement.setAttribute("style", "--circle-color: #93301b");
+
+  if (newMode === 'dark') {
+    document.documentElement.setAttribute("style", "--circle-color: #93301b");
+  } else {
+    document.documentElement.setAttribute("style", "--circle-color: #ffb100");
+  }
+
   localStorage.setItem('mode', newMode);
 }
 
 document.getElementById('toggle').addEventListener('click', toggleLightDarkMode);
 
+function readLocalStorage() {
+
+  const blogData = JSON.parse(localStorage.getItem('blogPosts')) || [];
+  return blogData;
+}
+
 function storeLocalStorage(newPost) {
-  const existingPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+  const existingPosts = readLocalStorage('blogPosts');
+
   existingPosts.push(newPost);
+
   localStorage.setItem('blogPosts', JSON.stringify(existingPosts));
 }
 
-function readLocalStorage() {
-  return JSON.parse(localStorage.getItem('blogPosts')) || [];
-}
-
+let redirectURL = '';
 const redirectPage = function (url) {
+  redirectURL = url;
   location.assign(url);
 };
 
